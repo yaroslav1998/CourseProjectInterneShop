79e94e8f381eb30397996517dfa6b6115b40c5a3
ваше имя < >
2017-11-11 21:45:21 +0300

    Deleted useless models

--- Data/admin.py
+++ Data/admin.py
@@ -1,17 +1,59 @@
 from django.contrib import admin
 from Data.models import *
-admin.site.register(Article)
-admin.site.register(Category)
-admin.site.register(CategoryParameter)
-admin.site.register(Manufacturer)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(Product)
-admin.site.register(ProductArticle)
-admin.site.register(ProductParameterValue)
-admin.site.register(ProductRate)
-admin.site.register(ProductReview)
-admin.site.register(ProductSale)
-admin.site.register(Stock)
-admin.site.register(ProductStockBalance)
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in User._meta.fields]
+    class Meta:
+        model=User
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Category._meta.fields]
+    class Meta:
+        model=Category
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Manufacturer._meta.fields]
+    class Meta:
+        model=Manufacturer
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Order._meta.fields]
+    class Meta:
+        model=Order
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in OrderItem._meta.fields]
+    class Meta:
+        model=OrderItem
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
+    class Meta:
+        model=Product
+class ProductRateAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductRate._meta.fields]
+    class Meta:
+        model=ProductRate
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductReview._meta.fields]
+    class Meta:
+        model=ProductReview
+class ProductSaleAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductSale._meta.fields]
+    class Meta:
+        model=ProductSale
+class StockAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Stock._meta.fields]
+    class Meta:
+        model=Stock
+class ProductStockBalanceAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductStockBalance._meta.fields]
+    class Meta:
+        model=ProductStockBalance
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Manufacturer,ManufacturerAdmin)
+admin.site.register(Order,OrderAdmin)
+admin.site.register(OrderItem,OrderItemAdmin)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(ProductImage)
+admin.site.register(ProductRate,ProductRateAdmin)
+admin.site.register(ProductReview,ProductReviewAdmin)
+admin.site.register(ProductSale,ProductSaleAdmin)
+admin.site.register(Stock,StockAdmin)
+admin.site.register(ProductStockBalance,ProductStockBalanceAdmin)
+admin.site.register(User,UserAdmin)
--- Data/models.py
+++ Data/models.py
@@ -10,19 +10,6 @@ from __future__ import unicode_literals
 from django.db import models
 from datetime import datetime
 
-class Article(models.Model):
-    name = models.CharField(max_length=1024)
-    meta_title = models.CharField(max_length=1024)
-    meta_h1 = models.CharField(max_length=1024)
-    meta_description = models.TextField()
-    meta_keywords = models.CharField(max_length=1024)
-    description = models.TextField()
-    content = models.TextField()
-    date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'article'
 
 
 class Category(models.Model):
@@ -33,58 +20,46 @@ class Category(models.Model):
     meta_keywords = models.CharField(max_length=1024)
     content = models.TextField()
     pid = models.ForeignKey('Product', models.DO_NOTHING, db_column='pid')
-    img = models.CharField(max_length=1024)
-    sort_order = models.IntegerField()
-    link = models.CharField(max_length=1024)
-    enabled = models.IntegerField()
-
     class Meta:
         managed = False
         db_table = 'category'
-
-
-class CategoryParameter(models.Model):
-    name = models.CharField(max_length=1024)
-    cid = models.ForeignKey(Category, models.DO_NOTHING, db_column='cid')
-
-    class Meta:
-        managed = False
-        db_table = 'category_parameter'
-
+    def __str__(self):
+        return "%s" % self.name
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=1024)
     description = models.TextField()
-
     class Meta:
         managed = False
         db_table = 'manufacturer'
+    def __str__(self):
+        return "%s" % self.name
 
 
 class Order(models.Model):
     uid = models.ForeignKey('User', models.DO_NOTHING, db_column='uid')
     adress = models.TextField()
     phone = models.CharField(max_length=1024)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True,auto_now=False)
     status = models.IntegerField()
-
     class Meta:
         managed = False
         db_table = 'order'
+    def __str__(self):
+        return "%s %s" % (self.uid.name,self.id)
 
 
 class OrderItem(models.Model):
     oid = models.ForeignKey(Order, models.DO_NOTHING, db_column='oid')
     pid = models.ForeignKey('Product', models.DO_NOTHING, db_column='pid')
     count = models.DecimalField(max_digits=65, decimal_places=2)
-
     class Meta:
         managed = False
         db_table = 'order_item'
-
+    def __str__(self):
+        return "%s %s" % (self.oid,self.pid.name)
 
 class Product(models.Model):
-    id = models.ForeignKey('ProductArticle', models.DO_NOTHING, db_column='id', primary_key=True)
     name = models.CharField(max_length=1024)
     meta_title = models.CharField(max_length=1024)
     meta_h1 = models.CharField(max_length=1024)
@@ -92,46 +67,31 @@ class Product(models.Model):
     meta_keywords = models.CharField(max_length=1024)
     description = models.TextField()
     description_full = models.TextField()
-    cid = models.ForeignKey(Category, models.DO_NOTHING, db_column='cid')
     link = models.CharField(max_length=1024)
     enabled = models.IntegerField()
     price = models.DecimalField(max_digits=65, decimal_places=2)
     views = models.IntegerField()
     article = models.CharField(max_length=1024)
     manufacturer = models.ForeignKey(Manufacturer, models.DO_NOTHING)
-
     class Meta:
         managed = False
         db_table = 'product'
+    def __str__(self):
+        return "%s %s" % (self.id,self.name)
 
-
-class ProductArticle(models.Model):
-    pid = models.IntegerField()
-    aid = models.ForeignKey(Article, models.DO_NOTHING, db_column='aid')
-
-    class Meta:
-        managed = False
-        db_table = 'product_article'
-
-
-class ProductParameterValue(models.Model):
-    product = models.ForeignKey(Product, models.DO_NOTHING)
-    parameter = models.ForeignKey(CategoryParameter, models.DO_NOTHING)
-    value = models.TextField()
-
-    class Meta:
-        managed = False
-        db_table = 'product_parameter_value'
-
+class ProductImage(models.Model):
+    product=models.ForeignKey(Product,models.DO_NOTHING)
+    image=models.ImageField(upload_to="media/products_image/")
 
 class ProductRate(models.Model):
     id = models.ForeignKey(Product, models.DO_NOTHING, db_column='id', primary_key=True)
     rate = models.IntegerField()
     uid = models.ForeignKey('User', models.DO_NOTHING, db_column='uid')
-
     class Meta:
         managed = False
         db_table = 'product_rate'
+    def __str__(self):
+        return "%s %s" % (self.id.name,self.rate)
 
 
 class ProductReview(models.Model):
@@ -141,55 +101,51 @@ class ProductReview(models.Model):
     enabled = models.IntegerField()
     comment_admin = models.TextField()
     pid = models.ForeignKey(Product, models.DO_NOTHING, db_column='pid')
-
     class Meta:
         managed = False
         db_table = 'product_review'
-
+    def __str__(self):
+        return "%s %s" % (self.uid.name,self.comment)
 
 class ProductSale(models.Model):
     pid = models.ForeignKey(Product, models.DO_NOTHING, db_column='pid')
     price_old = models.DecimalField(max_digits=65, decimal_places=2)
-
+    sale_coef=models.IntegerField()
     class Meta:
         managed = False
         db_table = 'product_sale'
-
+    def __str__(self):
+        return "%s %s" % (self.pid.name,self.sale_coef)
 
 class ProductStockBalance(models.Model):
     pid = models.ForeignKey(Product, models.DO_NOTHING, db_column='pid')
     sid = models.ForeignKey('Stock', models.DO_NOTHING, db_column='sid')
     balance = models.DecimalField(max_digits=65, decimal_places=2)
-
     class Meta:
         managed = False
         db_table = 'product_stock_balance'
-
+    def __str__(self):
+        return "%s %s" % (self.pid.name,self.sale_coef)
 
 class Stock(models.Model):
     name = models.CharField(max_length=1024)
     adress = models.TextField()
     enabled = models.IntegerField()
-
     class Meta:
         managed = False
         db_table = 'stock'
-
-
+    def __str__(self):
+        return "%s %s" % (self.name,self.enabled)
 class User(models.Model):
-    email = models.CharField(max_length=1024)
+    email = models.EmailField(max_length=1024)
     password = models.CharField(max_length=1024)
     role = models.CharField(max_length=1024)
     reg_date = models.DateTimeField()
     name = models.CharField(max_length=1024)
     phone = models.CharField(max_length=1024)
     adress = models.CharField(max_length=1024)
-    reg_date=datetime.now()#автоматически записывает дату регистрации
-    def __str__(self):
-        try:
-            return "e-mail: %s Adress: %s"% (self.email, self.adress)
-        except:
-            return self.id
     class Meta:
         managed = False
         db_table = 'user'
+    def __str__(self):
+        return "%s %s" % (self.name,self.adress)
