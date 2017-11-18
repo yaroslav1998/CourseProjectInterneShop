a4b93023e4c6e0c0fefa291368fc3027ed8587ff
ваше имя < >
2017-11-18 18:11:01 +0300

    create save and post_save signals in order price

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.signals import post_save
+
 class Order(models.Model):
 
     adress = models.TextField()
@@ -8,6 +10,8 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True,auto_now=False)
     status = models.ForeignKey('Status',models.DO_NOTHING,db_column='status')
     uid = models.ForeignKey("landing.User", models.DO_NOTHING, db_column='uid')
+    full_price=models.DecimalField(max_digits=65,decimal_places=2,default=0)
+    is_active=models.BooleanField()
     class Meta:
         managed = False
         db_table = 'order'
@@ -15,15 +19,35 @@ class Order(models.Model):
         return "%s" % (self.id)
 
 class OrderItem(models.Model):
-    oid = models.ForeignKey(Order, models.DO_NOTHING, db_column='oid')
+    order = models.ForeignKey(Order, models.DO_NOTHING, db_column='oid')
     pid = models.ForeignKey("product.Product", models.DO_NOTHING, db_column='pid')
+    price_per_item=models.DecimalField(max_digits=65,decimal_places=2,default=0)
     count = models.IntegerField(default=1)
-    Full_price=models.IntegerField()
+    Full_price=models.DecimalField(max_digits=65,decimal_places=2,default=0)
+    is_active=models.BooleanField()
     class Meta:
         managed = False
         db_table = 'order_item'
+
     def __str__(self):
-        return "%s %s" % (self.oid,self.pid.name)
+        return "%s %s" % (self.order,self.pid.name)
+
+    def save(self,*args,**kwargs):
+        self.price_per_item=self.pid.price
+        self.Full_price = self.count*self.price_per_item
+        super(OrderItem, self).save(*args, **kwargs)
+
+def orderitem_post_save(sender, instance, created, **kwargs):
+    order = instance.order
+    products_in_order = OrderItem.objects.filter(order=order, is_active=True)
+    order_full_price = 0
+    for item in products_in_order:
+        order_full_price = order_full_price + item.Full_price
+    instance.order.full_price = order_full_price
+    instance.order.save(force_update=True)
+
+post_save.connect(orderitem_post_save, sender=OrderItem)
+
 
 class Status(models.Model):
     name=models.CharField(max_length=30)
--- product/models.py
+++ product/models.py
@@ -18,15 +18,15 @@ class Product(models.Model):
     meta_description = models.TextField()
     meta_keywords = models.CharField(max_length=1024)
     description = models.TextField()
-    enabled = models.IntegerField()
-    price = models.DecimalField(max_digits=65, decimal_places=2)
+    enabled = models.BooleanField()
+    price = models.DecimalField(max_digits=65, decimal_places=2,default=0)
     manufacturer=models.ForeignKey(Manufacturer,models.DO_NOTHING,db_column='manufacturer')
     category=models.ForeignKey('category.Category',models.DO_NOTHING,db_column='cid')
     class Meta:
         managed = False
         db_table = 'product'
     def __str__(self):
-        return "%s %s" % (self.id,self.name)
+        return "%s" % self.name
 
 class ProductImage(models.Model):
     product=models.ForeignKey(Product,models.DO_NOTHING)
@@ -35,9 +35,9 @@ class ProductImage(models.Model):
         return "%s" % self.image
 
 class ProductRate(models.Model):
-    id = models.OneToOneField(Product, models.DO_NOTHING, db_column='id', primary_key=True)
     rate = models.IntegerField()
     uid = models.ForeignKey('landing.User', models.DO_NOTHING, db_column='uid')
+    pid = models.ForeignKey(Product,models.DO_NOTHING,db_column='pid')
     class Meta:
         managed = False
         db_table = 'product_rate'
@@ -47,10 +47,8 @@ class ProductRate(models.Model):
 
 class ProductReview(models.Model):
     uid = models.ForeignKey('landing.User', models.DO_NOTHING, db_column='uid')
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True,auto_now=False)
     comment = models.TextField()
-    enabled = models.IntegerField()
-    comment_admin = models.TextField()
     pid = models.ForeignKey(Product, models.DO_NOTHING, db_column='pid')
     class Meta:
         managed = False
